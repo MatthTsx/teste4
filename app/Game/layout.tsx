766a747c4eb8e139de4711f79a9df55e92737c3f
Mateus <shakii876954@gmail.com>
2023-01-30 19:12:16 -0300

    a

--- app/Game/layout.tsx
+++ app/Game/layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { JSXElementConstructor } from 'react'
 
-function RootLayout({children}:{children:React.ReactElement}) {
+function RootLayout({children}:{children:React.ReactElement<any, string | JSXElementConstructor<any>>}) {
   return (
     <div className='h-full w-full flex justify-center'>
       {children}
