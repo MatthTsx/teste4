c18a33979449ca98942616ef2794218178ef706c
Mateus <shakii876954@gmail.com>
2023-01-30 19:27:54 -0300

    fg hsdfs

--- app/Game/layout.tsx
+++ app/Game/layout.tsx
@@ -1,11 +1,9 @@
 import React, { JSXElementConstructor, ReactElement } from 'react'
 
-function RootLayout({children}:{children:ReactElement<any, string | JSXElementConstructor<any>>}) {
+export default function RootLayout({children}:{children:ReactElement<any, string | JSXElementConstructor<any>>}) {
   return (
     <div className='h-full w-full flex justify-center'>
       {children}
     </div>
   )
 }
-
-export default RootLayout
